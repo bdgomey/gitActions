de9b1b64080d855cbfe8b2e5932f077247b1a31c
Brian Gomes <67698602+bdgomey@users.noreply.github.com>
2023-10-17 11:08:02 -0500

    added resources

--- main.tf
+++ main.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "main" {
   name = "terraform"
   location = "eastus"
 }
+
+resource "azurerm_resource_group" "main2" {
+  location = "eastus"
+  name = "terraform2"
+}
